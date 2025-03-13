cc4ce348200c2e0110105deae2a4ce829686472d
Pablo Alexis Suntaxi Heredia <pablo.suntaxi@digitalchallengers.com.mx>
2025-03-13 13:35:44 -0500

    first commit

---
+++ src/main/java/com/tesis/motor_procesos/MotorProcesosApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.motor_procesos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorProcesosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorProcesosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tesis/motor_procesos/MotorProcesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.motor_procesos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorProcesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
